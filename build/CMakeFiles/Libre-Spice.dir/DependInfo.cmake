b2aef3c761db9d03d688eb972a28ecbb59064fb5
Guillermo Rojas <apemangr@gmail.com>
2022-09-14 16:51:49 -0300

    Basic structure

---
+++ build/CMakeFiles/Libre-Spice.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "RC"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_RC
+  "C:/Users/Apeman/Desktop/libre-spice/src/wx.rc" "C:/Users/Apeman/Desktop/libre-spice/build/CMakeFiles/Libre-Spice.dir/src/wx.rc.obj"
+  )
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_RC
+  "WXUSINGDLL"
+  "_FILE_OFFSET_BITS=64"
+  "__WXMSW__"
+  )
+
+# The include file search paths:
+set(CMAKE_RC_TARGET_INCLUDE_PATH
+  "C:/msys64/mingw64/lib/wx/include/msw-unicode-3.2"
+  "C:/msys64/mingw64/include/wx-3.2"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Apeman/Desktop/libre-spice/src/main.cpp" "CMakeFiles/Libre-Spice.dir/src/main.cpp.obj" "gcc" "CMakeFiles/Libre-Spice.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Libre-Spice.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Libre-Spice.dir/src/main.cpp.obj"
+  "CMakeFiles/Libre-Spice.dir/src/main.cpp.obj.d"
+  "CMakeFiles/Libre-Spice.dir/src/wx.rc.obj"
+  "Libre-Spice.exe"
+  "Libre-Spice.exe.manifest"
+  "Libre-Spice.pdb"
+  "libLibre-Spice.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX RC)
+  include(CMakeFiles/Libre-Spice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
